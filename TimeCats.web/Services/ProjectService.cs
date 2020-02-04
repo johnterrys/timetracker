dc8c942248a1d653a3ad113aab2b07196311d8a8
johnterrys <johnterryhfm93@gmail.com>
2020-02-03 22:20:59 -0700

    Implement projects and project details.

---
+++ TimeCats.web/Services/ProjectService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TimeCats.Models;
+
+namespace TimeCats.Services
+{
+    public class ProjectService
+    {
+        private readonly TimeTrackerContext _context;
+
+        public ProjectService(TimeTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public Project AddProject(Project project)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return project;
+        }
+
+        public Project GetProjectById(int projectId)
+        {
+            return _context.Projects.FirstOrDefault(p => p.projectID == projectId);
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            return _context.Projects;
+        }
+
+        public IEnumerable<Project> GetProjectsByCourseId(int courseId)
+        {
+            return _context.Projects
+                .Where(p => p.CourseID == courseId);
+        }
+    }
+}
