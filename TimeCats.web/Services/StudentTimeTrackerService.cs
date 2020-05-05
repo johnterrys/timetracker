52e20e6f78ad62ab4eccb5dfad89c07b50aa908a
johnterrys <johnterryhfm93@gmail.com>
2020-05-05 16:13:56 -0600

    This is a test commit with only adding a comment.

--- TimeCats.web/Services/StudentTimeTrackerService.cs
+++ TimeCats.web/Services/StudentTimeTrackerService.cs
@@ -25,6 +25,7 @@ namespace TimeCats.Services
             user.userID = _timeTrackerContext.Users.Max(user => user.userID) + 1;
             _timeTrackerContext.Users.Add(user);
             _timeTrackerContext.SaveChanges();
+            // Test doing a commit.
         }
 
         public User GetUserByID(int userID)
